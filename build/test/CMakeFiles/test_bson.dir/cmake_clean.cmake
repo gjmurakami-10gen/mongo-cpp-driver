822ef39edb3b913d70fb9afe3f5916fefc4dda84
Tyler Brock <tyler.brock@gmail.com>
2013-12-30 17:01:30 -0500

    messing around

---
+++ build/test/CMakeFiles/test_bson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_bson.dir/bson/test_bson.cpp.o"
+  "test_bson.pdb"
+  "test_bson"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_bson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/test/CMakeFiles/test_mongo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_mongo.dir/mongo/test_mongo.cpp.o"
+  "test_mongo.pdb"
+  "test_mongo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_mongo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
